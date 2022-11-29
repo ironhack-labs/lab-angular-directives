e46902c00a415e127053d7b6ce3594de5067acdf
YehosuaEs <yehosuaes@gmail.com>
2022-11-29 10:24:27 +0100

    adding some styles

--- lab/src/app/components/todos/todos.component.ts
+++ lab/src/app/components/todos/todos.component.ts
@@ -19,7 +19,9 @@ export class TodosComponent {
     this.isComplete = false;
     this.doValue = 'Done';
     this.displayCard = '';
-    this.todos.push(new TodoList('papas', false));
+    // this.todos.push(new TodoList('papas', false));
+    // this.todos.push(new TodoList('asd', false));
+    // this.todos.push(new TodoList('qwe', false));
   }
 
   /* -----  Methods ---- */
